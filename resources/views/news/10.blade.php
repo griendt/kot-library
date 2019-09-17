d68d53e09d92ce521dce8c66f6a29eecd65d0e02
Alex van de Griendt <alex@e-cube.nl>
2019-09-17 19:16:03 +0200

    New news item

---
+++ resources/views/news/10.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.card')
+@section('card-header')
+17 september 2019
+@overwrite
+@section('card-detail')
+    Work has restarted on this website and the idea is to keep pushing it to get it to a public release.<br>
+    The biggest change made recently is an overhaul of the side bar, which is now collapsible to allow for easier mobile browsing.<br>
+    Additionally, more layouts and exploits were uploaded, and ideas exist for an interesting tagging system! Stay tuned for more.
+@overwrite
