912046feeb2f7ba5dd4913b59e4298a88f1aff1e
Alex van de Griendt <alex@e-cube.nl>
2019-09-17 17:23:07 +0200

    Collapsible side bars and new blue-gray layout

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,25 +1,28 @@
-$(document).ready(function () {
+// Hide submenus
+$('#body-row .collapse').collapse('hide');
 
-        $('#sidebar').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left');
 
-    $('#ul-sidebar').on('click', function () {
-        $('#ul-sidebar').toggleClass('active');
-    });
-    });
-//
-//     $('.page-sidebar-menu li').removeClass('active');
-//
-// // highlight submenu item
-//     $('li a[href="' + this.location.pathname + '"]').parent().addClass('active');
-//
-// // Highlight parent menu item.
-//     $('li a[href="' + this.location.pathname + '"]').parents('li').addClass('active');
-//
-//     $('li a[href="' + this.location.pathname + '"]').removeClass('collapsed');
-//
-//     $('li a[href="' + this.location.pathname + '"]').addClass('show');
-    // $('li a[href="' + this.location.pathname + '"]').parents('li').parent().removeClass('collapsed');
-    // $('li a[href="' + this.location.pathname + '"]').parents('li').parent().setAttribute('aria-expanded', 'true');
-// });
+// Collapse click
+$('[data-toggle=sidebar-collapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none', 1000);
+    $('.sidebar-submenu').toggleClass('d-none', 1000);
+    $('.submenu-icon').toggleClass('d-none', 1000);
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed', 1000);
+
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,23 +1,36 @@
-<nav id="sidebar">
-    <div class="sidebar-header">
-        <h3></h3>
-    </div>
+<!-- Sidebar -->
+<div id="sidebar-container" class="sidebar-expanded d-none d-md-block">
+    <!-- Bootstrap List Group -->
+    <ul class="list-group">
+        <a
+            href="#"
+            data-toggle="sidebar-collapse"
+            class="bg-dark list-group-item list-group-item-action d-flex align-items-center"
+        >
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span id="collapse-icon" class="fa"></span>
+            </div>
+        </a>
 
-    <ul class="ul-sidebar list-unstyled components">
-        {{--<p>Dummy Heading</p>--}}
         @php
-            $bullets = [
-                route('home') => 'Home',
-                route('content.base') => 'Bases',
-                route('content.info') => 'Info',
-                route('content.guides') => 'Guides',
-                route('external.telegram') => 'Telegram',
-                route('stats') => 'Site Stats',
-                route('about') => 'About',
+        $bullets = [
+            route('home') => 'Home',
+            route('content.base') => 'Bases',
+            route('content.info') => 'Info',
+            route('content.guides') => 'Guides',
+            route('external.telegram') => 'Telegram',
+            route('stats') => 'Site Stats',
+            route('about') => 'About',
         ];
+
         foreach ($bullets as $url => $name) {
-            echo "<li><a href='$url'>$name</a></li>";
+            echo "
+            <a href='$url' class='bg-dark list-group-item list-group-item-action'>
+                <div class='d-flex w-100 justify-content-start align-items-center'>
+                    <span class='menu-collapsed'>$name</span>
+                </div>
+            </a>";
         }
         @endphp
     </ul>
-</nav>
+</div>
